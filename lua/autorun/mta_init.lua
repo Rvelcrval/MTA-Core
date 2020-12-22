594ce771cd2813ac61df4ab00b501655cbd7d8a3
Earu <earu.derg@gmail.com>
2020-12-22 21:45:35 +0100

    fuk

--- lua/autorun/mta_init.lua
+++ lua/autorun/mta_init.lua
@@ -15,11 +15,11 @@ include("mta/bombs.lua")
 -- riot shield
 do
 	-- apparently the texture manager needs to be loaded before the entities
-	AddCSLuaFile("mta/riot_shield/riot_shield.texture_manager.lua")
+	AddCSLuaFile("mta/riot_shield/riot_shield_texture_manager.lua")
 	AddCSLuaFile("mta/riot_shield/riot_shield.lua")
 	AddCSLuaFile("mta/riot_shield/riot_shield_table.lua")
 
-	include("mta/riot_shield/riot_shield.texture_manager.lua")
+	include("mta/riot_shield/riot_shield_texture_manager.lua")
 	include("mta/riot_shield/riot_shield.lua")
 	include("mta/riot_shield/riot_shield_table.lua")
 end
