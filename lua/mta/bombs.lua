e46c1aee7fe6b116301f6e2d412f1a7d22bd095d
unknown <earu.derg@gmail.com>
2021-01-01 23:07:41 +0100

    a

--- lua/mta/bombs.lua
+++ lua/mta/bombs.lua
@@ -57,6 +57,7 @@ if SERVER then
 	function MTA.TeleportBombToPlayer(ply)
 		local bomb = ents.Create("grenade_helicopter")
 		bomb:SetCollisionGroup(COLLISION_GROUP_PASSABLE_DOOR)
+		bomb:SetNotSolid(true)
 		bomb:Spawn()
 		bomb:SetNWBool("MTACombine", true)
 		bomb:SetNWBool("MTABomb", true)
