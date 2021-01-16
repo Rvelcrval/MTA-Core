0b7de4f185ccf4d82d8580c73c288bbcb94af91c
Earu <earu.derg@gmail.com>
2021-01-16 13:42:52 +0100

    fix jukebox maybe

--- lua/entities/mta_jukebox.lua
+++ lua/entities/mta_jukebox.lua
@@ -40,9 +40,7 @@ if SERVER then
 	end
 
 	function ENT:Use(activator)
-		if not activator:IsPlayer() then
-			return
-		end
+		if not activator:IsPlayer() then return end
 
 		net.Start(NET_JUKEBOX)
 		net.Send(activator)
@@ -63,11 +61,28 @@ if CLIENT then
 		MTA.RegisterEntityForHighlight(self, text, color_white)
 	end
 
+	file.CreateDir("mta")
+	local function get_song(url, name, cb)
+		cb = cb or function() end
+
+		if file.Exists("mta/" .. name, "DATA") then
+			file.Delete("mta/" .. name)
+		end
+
+		http.Fetch(url, function(body, _, _, code)
+			if code == 200 and body then
+				file.Write("mta/" .. name, body)
+				cb()
+			end
+		end)
+	end
+
+	local UI_STATION
 	local function show_jukebox_ui()
 		local frame = vgui.Create("DFrame")
 		frame:SetTitle("MTA Jukebox")
 		frame:ShowCloseButton(false)
-		frame:SetSize(500, 300)
+		frame:SetSize(500, 250)
 		frame:Center()
 
 		function frame:Paint(w, h)
@@ -76,6 +91,13 @@ if CLIENT then
 
 			surface.SetDrawColor(0, 0, 0, 210)
 			surface.DrawRect(0, 25, self:GetWide(), self:GetTall() - 25)
+		end
+
+		function frame:OnClose()
+			if IsValid(UI_STATION) then
+				UI_STATION:Stop()
+				UI_STATION = nil
+			end
 		end
 
 		frame:MakePopup()
@@ -90,7 +112,7 @@ if CLIENT then
 
 		-- Song selection
 		local songs = frame:Add("DPanel")
-		songs:SetSize(frame:GetWide() - 20, 200)
+		songs:SetSize(frame:GetWide() - 20, 150)
 		songs:Dock(TOP)
 		function songs:Paint( w, h)
 			surface.SetDrawColor(100, 100, 100, 200)
@@ -99,12 +121,15 @@ if CLIENT then
 
 		local songs_label = songs:Add("DLabel")
 		songs_label:SetPos(10, 10)
-		songs_label:SetText(
+		--[[songs_label:SetText(
 			"Select and preview songs that play during police assault!\nYou can also randomize the song that plays with shuffle.\nWith custom song slots you can add/remove your own songs."
+		)]]--
+		songs_label:SetText(
+			"Select and preview songs that play during police assault!\nWith custom song slots you can add/remove your own songs."
 		)
 		songs_label:SizeToContents()
 
-		local songs_shuffle = songs:Add("DCheckBox")
+		--[[local songs_shuffle = songs:Add("DCheckBox")
 		songs_shuffle:SetSize(15, 15)
 		songs_shuffle:SetPos(10, 65)
 		songs_shuffle:SetChecked(false)
@@ -121,18 +146,18 @@ if CLIENT then
 		local songs_shuffle_label = songs:Add("DLabel")
 		songs_shuffle_label:SetPos(30, 65)
 		songs_shuffle_label:SetTextColor(Color(255, 255, 255))
-		songs_shuffle_label:SetText("Shuffle")
-		songs_shuffle_label:SizeToContents()
+		songs_shuffle_label:SetText("SHUFFLE")
+		songs_shuffle_label:SizeToContents()]]--
 
 		local songs_combobox = songs:Add("DComboBox")
 		songs_combobox:SetSize(300, 20)
-		songs_combobox:SetPos(10, 100)
+		songs_combobox:SetPos(10, 50)
 		songs_combobox:SetPaintBackground(false)
 		songs_combobox:SetTextColor(Color(255, 255, 255))
 		songs_combobox:SetSortItems(false)
 
-		for _, url in pairs(MTA.Songs) do
-			songs_combobox:AddChoice(url)
+		for i, url in pairs(MTA.Songs) do
+			songs_combobox:AddChoice(("Song - %d"):format(i), { URL = url, ID = i })
 		end
 
 		function songs_combobox:Paint(w, h)
@@ -142,7 +167,7 @@ if CLIENT then
 
 		local songs_preview_btn = songs:Add("DButton")
 		songs_preview_btn:SetSize(60, 20)
-		songs_preview_btn:SetPos(250, 75)
+		songs_preview_btn:SetPos(330, 50)
 		songs_preview_btn:SetText("PREVIEW")
 		local preview_is_playing = false
 		function songs_preview_btn:Paint(w, h)
@@ -153,13 +178,32 @@ if CLIENT then
 			self:SetTextColor(col)
 		end
 		function songs_preview_btn:DoClick()
+			if not preview_is_playing then
+				local _, data = songs_combobox:GetSelected()
+				local file_path = ("song_slot_%d.dat"):format(data.ID)
+				get_song(data.URL, file_path, function()
+					sound.PlayFile(file_path, "", function(music, code, err)
+						print("song", code, err)
+						if not IsValid(music) then return end
+						UI_STATION = music
+						UI_STATION:Play()
+						UI_STATION:SetVolume(1)
+					end)
+				end)
+			else
+				if IsValid(UI_STATION) then
+					UI_STATION:Stop()
+					UI_STATION = nil
+				end
+			end
+
 			preview_is_playing = not preview_is_playing
 
 			local text = (preview_is_playing) and "STOP" or "PREVIEW"
 			self:SetText(text)
 		end
 
-		local songs_select_btn = songs:Add("DButton")
+		--[[local songs_select_btn = songs:Add("DButton")
 		songs_select_btn:SetSize(50, 20)
 		songs_select_btn:SetPos(340, 100)
 		songs_select_btn:SetTextColor(Color(255, 255, 255, 255))
@@ -174,11 +218,11 @@ if CLIENT then
 
 			local col = (self:IsHovered() and self:IsEnabled()) and Color(255, 200, 100, 255) or Color(255, 255, 255, alpha)
 			self:SetTextColor(col)
-		end
+		end]]--
 
 		local songs_remove_btn = songs:Add("DButton")
 		songs_remove_btn:SetSize(50, 20)
-		songs_remove_btn:SetPos(400, 100)
+		songs_remove_btn:SetPos(400, 50)
 		songs_remove_btn:SetTextColor(Color(255, 255, 255))
 		songs_remove_btn:SetText("REMOVE")
 		function songs_remove_btn:Paint(w, h)
@@ -189,9 +233,20 @@ if CLIENT then
 			self:SetTextColor(col)
 		end
 
+		function songs_remove_btn:DoClick()
+			local _, data = songs_combobox:GetSelected()
+			if not data then return end
+			if not data.URL or data.URL:Trim() == "" then return end
+
+			net.Start(NET_SONGS_TRANSMIT)
+			net.WriteString("delete")
+			net.WriteString(data.URL)
+			net.SendToServer()
+		end
+
 		local songs_customs_textentry = songs:Add("DTextEntry")
 		songs_customs_textentry:SetSize(300, 20)
-		songs_customs_textentry:SetPos(10, 160)
+		songs_customs_textentry:SetPos(10, 120)
 		songs_customs_textentry:SetPlaceholderText("Type url here..") -- This didn't even work by itself so uhh
 		songs_customs_textentry:SetPaintBackground(false)
 		function songs_customs_textentry:Paint(w, h)
@@ -214,14 +269,18 @@ if CLIENT then
 		end
 
 		local songs_customs_textentry_label = songs:Add("DLabel")
-		songs_customs_textentry_label:SetPos(10, 140)
-		songs_customs_textentry_label:SetText(
-			"You have " .. MTA.GetPlayerStat("prestige_level") .. " slots for custom songs left."
-		)
+		songs_customs_textentry_label:SetPos(10, 100)
+		songs_customs_textentry_label:SetText("You have 0 slots for custom songs left.")
 		songs_customs_textentry_label:SizeToContentsX(10)
+		function songs_customs_textentry_label:Think()
+			local remaining_slots = math.max(MTA.GetPlayerStat("prestige_level") - #MTA.Songs, 0)
+			local text = ("You have %d remaining slots for custom songs left."):format(remaining_slots)
+			self:SetText(text)
+			self:SizeToContentsX(10)
+		end
 
 		local songs_customs_add_btn = songs:Add("DButton")
-		songs_customs_add_btn:SetPos(340, 160)
+		songs_customs_add_btn:SetPos(330, 120)
 		songs_customs_add_btn:SetSize(40, 20)
 		songs_customs_add_btn:SetText("ADD")
 		function songs_customs_add_btn:Paint(w, h)
@@ -230,6 +289,20 @@ if CLIENT then
 
 			local col = (self:IsHovered()) and Color(255, 200, 100) or Color(255, 255, 255)
 			self:SetTextColor(col)
+		end
+
+		function songs_customs_add_btn:DoClick()
+			local url = songs_customs_textentry:GetText()
+			if not url or url:Trim() == "" then return end
+
+			net.Start(NET_SONGS_TRANSMIT)
+			net.WriteString("add")
+			net.WriteString(url)
+			net.SendToServer()
+		end
+
+		function songs_customs_textentry:OnEnter()
+			songs_customs_add_btn:DoClick()
 		end
 
 		-- Visualizer
@@ -250,7 +323,7 @@ if CLIENT then
 			surface.SetDrawColor(100, 100, 100, 200)
 			surface.DrawOutlinedRect(0, 0, w, h, 3)
 
-			local station = MTA.SongStation
+			local station = IsValid(UI_STATION) and UI_STATION or MTA.SongStation
 
 			if IsValid(station) then
 				station:FFT(FFT_DATA, FFT_256)
