b2540e9d23eb7a214a334f3f169788056cacbf05
Earu <earu.derg@gmail.com>
2021-01-23 19:11:53 +0100

    this will hopefully make everything a bit more epic and manageable

--- lua/autorun/mta_init.lua
+++ lua/autorun/mta_init.lua
@@ -15,3 +15,18 @@ include("mta/bounties.lua")
 include("mta/bombs.lua")
 include("mta/fever.lua")
 include("mta/riot_shield_texture_manager.lua")
+
+-- better combine tracers
+do
+	if SERVER then
+		-- commenting this for now, so we get the latest update from the workshop addon
+		-- for the particles (.pcf) instead of having a dated copy
+		-- resource.AddFile("particles/cmb_tracers_rework.pcf")
+	end
+
+	PrecacheParticleSystem("cmb_tracer")
+	PrecacheParticleSystem("ar2_combineball")
+
+	game.AddParticles("particles/cmb_tracers_rework.pcf")
+	game.AddParticles("particles/cmb_tracers_rework.pcf")
+end
---
+++ lua/effects/combineball/init.lua
@@ -0,0 +1,13 @@
+local TRACK_CB = {}
+hook.Add("Tick", "CheckCB", function()
+	local cb = ents.FindByClass("prop_combine_ball")
+	for k, v in pairs(cb) do
+		if not(TRACK_CB[v]) then
+			TRACK_CB[v] = {true, v:GetPos(), CurTime()}
+			ParticleEffectAttach("ar2_combineball", PATTACH_ABSORIGIN_FOLLOW, v, 0)
+		else
+			TRACK_CB[v][1] = true
+			TRACK_CB[v][2] = v:GetPos()
+		end
+	end
+end)
