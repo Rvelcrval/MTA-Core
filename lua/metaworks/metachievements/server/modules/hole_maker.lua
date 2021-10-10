e051681f71f43b7e5ec1452b25909bb0118fcca0
Earu <earu.derg@gmail.com>
2021-10-10 17:48:43 +0200

    im on fire today

---
+++ lua/metaworks/metachievements/server/modules/hole_maker.lua
@@ -0,0 +1,17 @@
+if not MetAchievements then return end
+
+local tag = "MetAchievements"
+local id = "hole_maker"
+
+resource.AddFile("materials/metachievements/" .. id .. "/s1/icon.png")
+
+MetAchievements.RegisterAchievement(id, {
+	title = "Hole Maker",
+	description = "You like to make holes, even in things that can get you in troubles!"
+})
+
+local hook_name = ("%s_%s"):format(tag, id)
+hook.Add("MTADrillSuccess", hook_name, function(ply)
+	if MetAchievements.HasAchievement(ply, id) then return end
+	MetAchievements.UnlockAchievement(ply, id)
+end)
